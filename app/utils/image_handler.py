e8ef4f48937323ade9e2f389dd1e711d8333f380
farhanrhine <mohammadfarhanalam09@gmail.com>
2026-03-02 08:03:24 +0530

    fix: Add type ignore comments for compatibility in image processing functions

--- app/utils/image_handler.py
+++ app/utils/image_handler.py
@@ -10,21 +10,21 @@ def process_image(image_file):
     nparr = np.frombuffer(image_bytes,np.uint8)
 
     img = cv2.imdecode(nparr,cv2.IMREAD_COLOR)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # type: ignore
 
-    face_cascade =cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    face_cascade =cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml') # type: ignore
 
     faces =face_cascade.detectMultiScale(gray,1.1,5)
 
     if len(faces)==0:
         return image_bytes,None
 
-    largest_face = max(faces,key=lambda r:r[2] *r[3])
+    largest_face = max(faces,key=lambda r:r[2] *r[3]) # Picking the largest face (width × height)
 
     (x,y,w,h) = largest_face
 
-    cv2.rectangle(img, (x,y),(x+w , y+h) , (0,255,0),3 )
+    cv2.rectangle(img, (x,y),(x+w , y+h) , (255,0,255),3 ) # type: ignore  Drawing the green rectangle
 
-    is_sucess , buffer = cv2.imencode(".jpg" , img)
+    is_sucess , buffer = cv2.imencode(".jpg" , img) # type: ignore
 
     return buffer.tobytes(), largest_face
